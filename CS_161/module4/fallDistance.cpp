dfe7708ae2f0a1adab3979f8a6a6786baec22800
P-dev <pdev@ubuntu.(none)>
2016-04-19 03:17:10 -0700

    - Homework 4 assignment

--- CS_161/module4/fallDistance.cpp
+++ CS_161/module4/fallDistance.cpp
@@ -1,14 +1,14 @@
 /*********************************************************************
 ** Author:       Jeremy Prater
 ** Date:         April 18, 2016
-** Description:  Module 4.a assignment
+** Description:  Module 4.b assignment
 *********************************************************************/
 
 #include <iostream>
 #include <cmath>
 
 /*********************************************************************
-** Description: Assignment 4.a
+** Description: Assignment 4.b
 **
 ** Write a function called fallDistance that takes time as an
 ** argument and returns distance.
@@ -31,14 +31,16 @@
 
 float fallDistance (float time)
 {
-  return (1.0f/2)(9.8f)(pow(time,2));
+  return (1.0/2)*(9.8)*pow(time,2);
 }
 
 #ifdef Debug
-void main(void)
+int main(void)
 {
-  cout << fallDistance (1.0f) << endl;
-  cout << fallDistance (2.0f) << endl;
-  cout << fallDistance (3.0f) << endl;
+  std::cout << fallDistance (1.0) << std::endl;
+  std::cout << fallDistance (2.0) << std::endl;
+  std::cout << fallDistance (3.0) << std::endl;
+
+  return 0;
 }
 #endif

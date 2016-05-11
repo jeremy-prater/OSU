b6dcc741b486af604e41e391624e89bb6f5d565f
Jeremy Prater <jeremy.prater1@hp.com>
2016-05-11 11:00:56 -0700

    - Module 7 testing

--- CS_161/module7/findMode.cpp
+++ CS_161/module7/findMode.cpp
@@ -49,19 +49,16 @@ std::vector<int> findMode (int input[], int length)
 
   for (int i=0; i<length; i++)
   {
-    printf ("looper: %d\n", i);
     struct int_frequency temp_storage;
 
     // Find or create a new frequency counter for input[i]
     bool found = false;
     for (int x = 0; x < temp_data.size(); x++)
     {
-      printf ("Start %d\n", x);
       if (temp_data[x].value == input[i])
       {
         // Node found.
         temp_data[x].count++;
-        printf ("Mod %d:%d\n", temp_data[x].value, temp_data[x].count);
         found = true;
       }
     }
@@ -70,7 +67,6 @@ std::vector<int> findMode (int input[], int length)
       // Create and assign node.
       temp_storage.value = input[i];
       temp_storage.count = 1;
-      printf ("Adding %d:%d\n", temp_storage.value, temp_storage.count);
       temp_data.push_back(temp_storage);
     }
   }
@@ -101,6 +97,14 @@ std::vector<int> findMode (int input[], int length)
 
 
 #ifdef Debug
+void debug_printf(std::vector<int> result)
+{
+  for (int x=0;x<result.size();x++)
+  {
+    printf ("loop: %d=%d\n",x ,result[x]);
+  }
+}
+
 int main()
 {
   int test1[] = {1,1,1,1,1,2};
@@ -108,22 +112,10 @@ int main()
   int test3[] = {1,2,3,4,5,6};
   int test4[] = {1,1,2,2,3,3};
 
-  printf ("Starting...\n");
-
-  std::vector<int> result1 = findMode(test1, ARRAY_LENGTH (test1));
-  printf ("complete\n");
-  for (int x=0;x<result1.size();x++)
-  {
-    printf ("loop: %d=%d\n",x ,result1[x]);
-  }
-
-  //std::vector<int> result2 = findMode(test1, sizeof (test2));
-  //std::vector<int> result3 = findMode(test1, sizeof (test3));
-  //std::vector<int> result3 = findMode(test1, sizeof (test4));
-
-
-  //std::cout << "Distance : " << dist << std::endl;
-
+  debug_printf (findMode(test1, ARRAY_LENGTH (test1)));
+  debug_printf (findMode(test1, ARRAY_LENGTH (test2)));
+  debug_printf (findMode(test1, ARRAY_LENGTH (test3)));
+  debug_printf (findMode(test1, ARRAY_LENGTH (test4)));
   return 0;
 }
 #endif

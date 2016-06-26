aefb8048819f4b7d26921d7b413d42cc65a65920
Jeremy Prater <jeremy.prater1@hp.com>
2016-06-25 21:44:02 -0700

    - Update lab-aa for new assignment.

--- CS_162/lab_aa/lab-aa.cpp
+++ CS_162/lab_aa/lab-aa.cpp
@@ -4,15 +4,14 @@
 ** Description:  Lab Aa
 *********************************************************************/
 
-#include "summation.h"
+#include "average.h"
 #include "lib_flip_display.h"
 #include <stdlib.h>
 
-
 /*********************************************************************
-** Description: Lab Aa lab1-2.cpp
+** Description: Lab Aa lab1-1.cpp
 **
-** Implementation for lab1-2.cpp
+** Implementation for lab1-1.cpp
 **
 *********************************************************************/
 
@@ -20,27 +19,58 @@
 //
 // int main (void)
 //
-// Program entry point for lab1-2.cpp
+// Program entry point for lab1-1.cpp
 //
 
 int main (void)
 {
   init_display();
-  debug_print (true, COLOR_GREEN, "CS_162: Lab Aa 1-2 Testing...\n\n");
-  
+  debug_print (true, COLOR_GREEN, "CS_162: Lab Aa 1-1 Testing...\n\n");
+
   double testResult;
+
   double testArray1[10] = {0,1,2,3,4,5,6,7,8,9};
   double testArray2[10] = {-5,-4,-3,-2,-1,1,2,3,4,5};
-    
+
   unsigned int userArrayLength;
   double * userArray;
-  
+
+  debug_print (false, COLOR_WHITE, "Average of zero length array...");
+  debug_test_result (avg (testArray1, 0) == 0);
+
+  debug_print (false, COLOR_WHITE, "Average of nullptr array...");
+  debug_test_result (avg (__null, ARRAY_SIZE (testArray1)) == 0);
+
+  debug_print (false, COLOR_WHITE, "Average of testArray1...");
+  debug_test_result (avg (testArray1, ARRAY_SIZE (testArray1)) == 4.5);
+
+  debug_print (false, COLOR_WHITE, "Average of testArray1 with incorrect length...");
+  debug_test_result (avg (testArray1, ARRAY_SIZE (testArray1)-1) != 4.5);
+
+  debug_print (false, COLOR_WHITE, "Average of testArray2...");
+  debug_test_result (avg (testArray2, ARRAY_SIZE (testArray2)) == 0);
+
+  debug_print (false, COLOR_WHITE,"Enter user defined array test:\n\nEnter number of elements: ");
+
+  scanw ("%u", &userArrayLength);
+
+  userArray = (double*)malloc (userArrayLength * sizeof (double));
+  for (unsigned int userCount = 0;userCount < userArrayLength; userCount++)
+  {
+    debug_print (false, COLOR_WHITE,"Enter data (%u/%u): ", userCount+1, userArrayLength);
+    scanw ("%lf", &userArray[userCount]);
+  }
+
+  debug_print (false, COLOR_WHITE, "User Average : %lf\n\n", avg(userArray, userArrayLength));
+
+  free (userArray);
+
   debug_print (false, COLOR_WHITE, "Summation of zero length array...");
   debug_test_result (sum (testArray1, 0) == 0);
 
   debug_print (false, COLOR_WHITE, "Summation of nullptr array...");
   debug_test_result (sum (__null, ARRAY_SIZE (testArray1)) == 0);
-  
+
   debug_print (false, COLOR_WHITE, "Summation of testArray1...");
   debug_test_result (sum (testArray1, ARRAY_SIZE (testArray1)) == 45);
 

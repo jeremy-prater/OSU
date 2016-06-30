e4e2d2924d9a97cfc1340a1f52d6355673916524
Jeremy Prater <jeremy.prater1@hp.com>
2016-06-29 17:03:15 -0700

    - Add more test cases

--- CS_162/assignment1/groceryMain.cpp
+++ CS_162/assignment1/groceryMain.cpp
@@ -29,32 +29,61 @@ using namespace std;
 int main()
 {
   init_display();
-  debug_print (true, COLOR_CYAN, "Grocery List V1.0 Testing...\n\n");
+  debug_print (true, COLOR_CYAN, "Grocery List V1.0 Testing...\t\t\n\n");
 
   groceryList * myList = new groceryList();
 
-  debug_print (false, COLOR_WHITE, "Remove all items from empty storage.\t\t\t\n");
+  debug_print (false, COLOR_WHITE, "Remove all items from empty storage.\t\t");
   myList->RemoveAllListItems();
   debug_test_result (myList->GetItemCount() == 0);
 
-  debug_print (false, COLOR_WHITE, "Remove item(0) from empty storage.\t\t\t\n");
-  myList->RemoveListItem(0);
-  debug_test_result (myList->GetItemCount() == 0);
-
-  debug_print (false, COLOR_WHITE, "Total price from empty storage.\t\t\t\n");
+  debug_print (false, COLOR_WHITE, "Total price from empty storage.\t\t\t");
   debug_test_result (myList->GetTotalPrice() == 0);
 
 
+  debug_print (false, COLOR_WHITE, "Adding item 1\t\t\t\t\t");
   myList->AddListItem ("Bananas", "lb", 0.99, 2.32);
-  myList->AddListItem ("Tomatoes", "ea", 0.99, 4);
-  myList->AddListItem ("Life cereal", "box", 3.19, 1);
-  myList->AddListItem ("Frozen pizza", "ea", 6.79, 2);
-  myList->AddListItem ("Apples", "lb", 0.79, 180);
-  myList->AddListItem ("Ice cream", "pack", 5.19, 1);
+  debug_test_result (myList->GetItemCount() == 1);
 
+  debug_print (false, COLOR_WHITE, "Adding item 2\t\t\t\t\t");
+  myList->AddListItem ("Tomatoes", "ea", 0.99, 4);
+  debug_test_result (myList->GetItemCount() == 2);
+
+  debug_print (false, COLOR_WHITE, "Adding item 3\t\t\t\t\t");
+  myList->AddListItem ("Life cereal", "box", 3.19, 1);
+  debug_test_result (myList->GetItemCount() == 3);
+
+  debug_print (false, COLOR_WHITE, "Adding item 4\t\t\t\t\t");
+  myList->AddListItem ("Frozen pizza", "ea", 6.79, 2);
+  debug_test_result (myList->GetItemCount() == 4);
+
+  debug_print (false, COLOR_WHITE, "Adding item 5\t\t\t\t\t");
+  myList->AddListItem ("Apples", "lb", 0.79, 180);
+  debug_test_result (myList->GetItemCount() == 5);
+
+  debug_print (false, COLOR_WHITE, "Adding item 6\t\t\t\t\t");
+  myList->AddListItem ("Ice cream", "pack", 5.19, 1);
+  debug_test_result (myList->GetItemCount() == 6);
+
+
+  debug_print (false, COLOR_WHITE, "Remove item(7) from used storage (out of index).");
+  myList->RemoveListItem(7);
+  debug_test_result (myList->GetItemCount() == 6);
+
+
+  debug_print (false, COLOR_WHITE, "Remove item(1) from used storage.\t\t");
   myList->RemoveListItem(1);
+  debug_test_result (myList->GetItemCount() == 5);
 
   myList->PrintGroceryList();
+
+  debug_print (false, COLOR_WHITE, "Remove all items from used storage.\t\t");
+  myList->RemoveAllListItems();
+  debug_test_result (myList->GetItemCount() == 0);
+
+  debug_print (false, COLOR_WHITE, "Adding item 1\t\t\t\t\t");
+  myList->AddListItem ("Bananas", "lb", 0.99, 2.32);
+  debug_test_result (myList->GetItemCount() == 1);
 
   delete myList;
   debug_print (false, COLOR_WHITE, "Test Complete! Press any key to exit.");

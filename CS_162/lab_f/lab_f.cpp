b4055474b3d2845814e2db6cb258c34212047d81
Jeremy Prater <jeremy.prater1@hp.com>
2016-07-31 14:15:03 -0700

    - Update testing logic for lab-f

--- CS_162/lab_f/lab_f.cpp
+++ CS_162/lab_f/lab_f.cpp
@@ -49,106 +49,122 @@ int main(int argc, char * argv[])
       DebugConsole::SetDebugLevel (1);
     }
 
+    DebugConsole::debug_print (0, true, COLOR_WHITE, "Starting Automated tests...\n");
+
     // Create a stack and a queue
 
-    Queue<int> myQueue;
-    Stack<int> myStack;
+    Queue<int> * myQueue = new Queue<int>();
+    Stack<int> * myStack = new Stack<int>();
 
     // Test functions on empty stack/queue
     // Check for segment violation
 
-    myQueue.getFront();
-    myQueue.removeFront();
+    myQueue->getFront();
+    myQueue->removeFront();
     
-    myStack.peek();
-    myStack.pop();
+    myStack->peek();
+    myStack->pop();
 
     // Populate data
 
-    myStack.push (1);
-    myStack.push (2);
-    myStack.push (3);
+    myStack->push (1);
+    myStack->push (2);
+    myStack->push (3);
 
     // This should pop the number 3
 
-    DebugConsole::debug_print (0, true, COLOR_WHITE, "stack pop: %d\n", myStack.peek());
-    myStack.pop();
+    DebugConsole::debug_print (0, true, COLOR_WHITE, "stack pop: %d\n", myStack->peek());
+    myStack->pop();
 
     // push more data
 
-    myStack.push (4);
-    myStack.push (5);
+    myStack->push (4);
+    myStack->push (5);
 
     // Print all data, empty stack
 
-    while (!myStack.isEmpty())
+    while (!myStack->isEmpty())
     {
-      DebugConsole::debug_print (0, true, COLOR_WHITE, "stack pop: %d\n", myStack.peek());
-      myStack.pop();
+      DebugConsole::debug_print (0, true, COLOR_WHITE, "stack pop: %d\n", myStack->peek());
+      myStack->pop();
     }
 
     // Push more items onto stack
 
-    myStack.push (6);
-    myStack.push (7);
-    myStack.push (8);
+    myStack->push (6);
+    myStack->push (7);
+    myStack->push (8);
     
     // Add items to the queue
 
-    myQueue.addBack(1);
-    myQueue.addBack(2);
-    myQueue.addBack(3);
+    myQueue->addBack(1);
+    myQueue->addBack(2);
+    myQueue->addBack(3);
 
     // Get the first item from the queue and remove the first one
 
-    DebugConsole::debug_print (0, true, COLOR_WHITE, "queue getFront: %d\n", myQueue.getFront());
-    myQueue.removeFront();
+    DebugConsole::debug_print (0, true, COLOR_WHITE, "queue getFront: %d\n", myQueue->getFront());
+    myQueue->removeFront();
 
     // Add more items to the queue
 
-    myQueue.addBack(4);
-    myQueue.addBack(5);
+    myQueue->addBack(4);
+    myQueue->addBack(5);
 
     // Print all queue items
     // Empty the queue
 
-    while (!myQueue.isEmpty())
+    while (!myQueue->isEmpty())
     {
-      DebugConsole::debug_print (0, true, COLOR_WHITE, "queue getFront: %d\n", myQueue.getFront());
-      myQueue.removeFront();
+      DebugConsole::debug_print (0, true, COLOR_WHITE, "queue getFront: %d\n", myQueue->getFront());
+      myQueue->removeFront();
     }
 
     // Add new items to an empty queue
 
-    myQueue.addBack(6);
-    myQueue.addBack(7);
-    myQueue.addBack(8);
+    myQueue->addBack(6);
+    myQueue->addBack(7);
+    myQueue->addBack(8);
 
     // Print all items from queue and stack
     // Remove all stack/queue items.
 
-    while (!myStack.isEmpty())
+    while (!myStack->isEmpty())
     {
-      DebugConsole::debug_print (0, true, COLOR_WHITE, "stack pop: %d\n", myStack.peek());
-      myStack.pop();
+      DebugConsole::debug_print (0, true, COLOR_WHITE, "stack pop: %d\n", myStack->peek());
+      myStack->pop();
     }
-    while (!myQueue.isEmpty())
+    while (!myQueue->isEmpty())
     {
-      DebugConsole::debug_print (0, true, COLOR_WHITE, "queue getFront: %d\n", myQueue.getFront());
-      myQueue.removeFront();
+      DebugConsole::debug_print (0, true, COLOR_WHITE, "queue getFront: %d\n", myQueue->getFront());
+      myQueue->removeFront();
     }
 
     // Add more items to the stack and queues.
 
-    myStack.push (10);
-    myStack.push (11);
-    myStack.push (12);
-    myQueue.addBack(10);
-    myQueue.addBack(11);
-    myQueue.addBack(12);
+    myStack->push (10);
+    myStack->push (11);
+    myStack->push (12);
+    myQueue->addBack(10);
+    myQueue->addBack(11);
+    myQueue->addBack(12);
 
     // Leave the data in the stack and queue to test for proper clean up when the Stack/Queue class is destroyed.
-    // Exit and return success.
+    // Delete stack and queue to test class destructors.
+    delete myStack;
+    delete myQueue;
+    
+    DebugConsole::debug_print (0, true, COLOR_WHITE, "Automated tests complete. Entering user mode.\n");
 
+    // Create new class objects
+    myStack = new Stack<int>();
+    myQueue = new Queue<int>();
+
+
+    // Destroy class objects
+    delete myStack;
+    delete myQueue;
+
+    // Exit and return success.
     return 0;
 }

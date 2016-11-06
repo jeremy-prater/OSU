e52e314869de16527b5b8ccf3257a6ae9a002795
Jeremy Prater <praterj@oregonstate.edu>
2016-11-05 21:17:33 -0700

    - CS 271 Project 4 work

--- CS_271/project4/JeremyPraterProject04.asm
+++ CS_271/project4/JeremyPraterProject04.asm
@@ -11,35 +11,42 @@ INCLUDE Irvine32.inc
 
 .data
 messageTitle        BYTE  "            Integer Accumulator         by       Jeremy Prater", 0
+messageEC1          BYTE  "** EC1: Align output columns." ,0
+messageEC2          BYTE  "** EC2: Display more composites, but show them one page at a time.", 0
+
 messageEnterName    BYTE  "What is your name? ", 0
 messageHello        BYTE  "Hello, ", 0
-messageInstruction1 BYTE  "Enter a numbers between [-100,-1].", 0
-messageInstruction2 BYTE  "Enter a positive number to exit.", 0
-messageGetNumber    BYTE  ": Enter a number : ", 0
-messageNumberRange  BYTE  " is out of range.", 0
-messageTotal1       BYTE  "You entered " ,0
-messageTotal2       BYTE  " valid numbers." ,0
-messageSum          BYTE  "The sum of valid negative integers is : " ,0
-messageAvgInt       BYTE  "The integer average of the valid negative integers is : ", 0
-messageAvgFloat     BYTE  "The floating point average of the valid negative integers is : ", 0
-messageExit1        BYTE  "Good bye " ,0
-messageExit2        BYTE  "!" ,0
 
-messageEC1          BYTE  "** EC1: Number the lines during user input." ,0
-messageEC2          BYTE  "** EC2: Calculate and display the average in floating point to the nearest .001", 0
+messageInstruction1 BYTE  "Enter the number of composite numbers you would like to see.", 0
+messageInstruction2 BYTE  "The maximum number of composites to display is 400.", 0
+
+messageGetNumber    BYTE  "Enter the number number of composites to display [1, 400] : ", 0
+messageRangeError   BYTE  " is out of range. Try again.", 0
+
+messageExit1        BYTE  "Assembly language is pretty powerful.", 0
+messageExit2        BYTE  "I should make a real-time thread scheduler!" ,0
+messageExit3        BYTE  "Good bye ", 0
+messageExit4        BYTE  "!" ,0
+
+messageSpace        BYTE  " " ,0
 
 storageName         BYTE 30 DUP (?) ; Memory to store user name
 storageNameLength   DWORD ($-storageName)
 
-
-FloatRounder       WORD 1000     ; This is actually 1/1000 = .001. Used for float rounding
-FloatDividend      DWORD ?       ; first integer entered by user
-FloatDivisor       DWORD ?       ; second integer entered by user
+maxUserInput        DWORD 300000
+columnNumber        DWORD 0
+rowNumber           BYTE  0
+cNumbersFound       DWORD 0
 
 .code
-main PROC
 
-;Main procedure requirements
+;--------------------------------------------------------
+;
+; Display the introduction
+;
+;--------------------------------------------------------
+
+displayIntro PROC
 ; introduction
 	mov		edx, OFFSET messageTitle
 	call	WriteString
@@ -54,7 +61,16 @@ main PROC
 	call	WriteString
 	call	CrLf
 	call	CrLf
+	ret
+displayIntro ENDP
 
+;--------------------------------------------------------
+;
+; Get and print the user name
+;
+;--------------------------------------------------------
+
+getUsername PROC
 ; Get the user name
 	mov		edx, OFFSET messageEnterName
 	call	WriteString
@@ -69,9 +85,18 @@ main PROC
 	call    WriteString
 	call    CrLf
 
-SHOWINSTRUCTIONS:
+	ret
+getUsername ENDP
 
-; Display instructions
+;--------------------------------------------------------
+;
+; Display the instructions for the application
+;
+;--------------------------------------------------------
+
+displayInstructions PROC
+	; Display instructions
+
     mov     edx, OFFSET messageInstruction1
 	call    WriteString
 	call    CrLf
@@ -80,105 +105,226 @@ SHOWINSTRUCTIONS:
 	call    WriteString
 	call    CrLf
 
-	; Setup the Accumulator and Counter
-	mov     ebx, 0 ; This is the accumulator
-	mov     ecx, 0 ; This is the counter
+	ret
+displayInstructions ENDP
 
-NEXTINPUT:
+;--------------------------------------------------------
+;
+; Get user input
+;
+;--------------------------------------------------------
 
-; Get integer input
-    mov     eax, ecx ; Move the counter into eax
-	inc     eax      ; Add one for the user count
-	call    WriteDec
+getInput PROC
+
+USERINPUT:
     mov     edx, OFFSET messageGetNumber
 	call    WriteString
 	call    ReadInt                        ; Result is in EAX
 
-; Verify input is int the range [-100, -1]
-	; Input number is in EAX
-	cmp      eax, 0
-	jge      CALCRESULTS
-	cmp      eax, -100
-	jge      ACCUMULATEINT
+	; Check input and return to start if it is out of range
+	cmp     eax, 1
+	jl      INPUTINVALID
+	cmp     eax, maxUserInput
+	jg      INPUTINVALID
 
-	; The input is below -100 and above -1
-	; Print error and return to input
-    call     WriteInt
-	mov      edx, OFFSET messageNumberRange
-	call     WriteString
-	call     CrLf
-	jmp      NEXTINPUT
-	
-	;jl      SHOWINSTRUCTIONS
-	;cmp     eax, 46
-	;jg      SHOWINSTRUCTIONS
-ACCUMULATEINT:
-	
-	; Input is valid
-	; eax contains the user input
-	; ebx is the accumulator
-	; ecx is the counter
+	; The input is valid
+	jmp     INPUTVALID
 
-	add      ebx, eax
-	inc      ecx
-	jmp      NEXTINPUT
-
-CALCRESULTS:
-	; ebx contains the integer accumulator
-	; ecx contains the counter
-
-	; Print the total sum
-	mov      edx, OFFSET messageSum
-	call     WriteString
-	mov      eax, ebx
-	call     WriteInt
-	call     CrLf
-
-	; Print the integer average
-	; Move total into EAX
-	mov      eax, ebx
-	; Sign extend into EDX
-	cdq
-	; Preform signed division into eax:edx
-	idiv     ecx
-	; Print result from EAX
-	; We discard the remainder at this point by loading a string offset into EDX
-	mov     edx, OFFSET messageAvgInt
+INPUTINVALID:
+	; The invalid number is in eax, print the error and return to the start of user input
+    call    WriteDec
+	mov     edx, OFFSET messageRangeError
 	call    WriteString
-	call    WriteInt
 	call    CrLf
+	jmp     GETINPUT
+	
+INPUTVALID:
+	ret
+getInput ENDP
 
-	; Now print the floating point answer
-	; EBX - sum
-	; ECX - counter
-	; Preform floating point division
-	mov     FloatDividend, ebx
-	mov     FloatDivisor, ecx
-	fild    FloatDividend    ; Load first operand into ST(0) Convert DWORD->REAL
-	fidiv   FloatDivisor     ; Loadn and divide first/second and store result in ST(0)
-	fimul   FloatRounder     ; Multiply ST(0) by 1000
-	frndint                  ; Round the float number to integer
-	fidiv   FloatRounder     ; Divide ST(0) by 1000 (round to .001)
+;--------------------------------------------------------
+;
+; Display the exit text for the application
+;
+;--------------------------------------------------------
 
-	; Print float division messsage and result
-	mov     edx, OFFSET messageAvgFloat
-	call    WriteString
-	call    WriteFloat
-	call    CrLf
-
-
-GOODBYE:
+displayExit PROC
 ; Show goodbye
 	call    CrLf
 	call    CrLf
 	mov     edx, OFFSET messageExit1
 	call    WriteString
-	mov     edx, OFFSET storageName
-	call    WriteString
+	call    CrLf
 	mov     edx, OFFSET messageExit2
 	call    WriteString
 	call    CrLf
+	mov     edx, OFFSET messageExit3
+	call    WriteString
+	mov     edx, OFFSET storageName
+	call    WriteString
+	mov     edx, OFFSET messageExit4
+	call    WriteString
+	call    CrLf
+	ret
+displayExit ENDP
 
+;--------------------------------------------------------
+;
+; Pause the output and wait for user input
+;
+;--------------------------------------------------------
+
+PauseOutput PROC
+	call CrLf
+	call WaitMsg
+	call CrLf
+	call CrLf
+	ret
+PauseOutput ENDP
+
+;--------------------------------------------------------
+;
+; Display spaces based on the size of the number
+;
+;--------------------------------------------------------
+
+WriteColumnSpaces PROC
+	mov     edx, OFFSET messageSpace
+
+	;cmp     eax, 1000000
+	;jge     COLUMNSPACE1
+	;call    WriteString
+
+;COLUMNSPACE1:
+	cmp     eax, 100000
+	jge     COLUMNSPACE2
+	call    WriteString
+
+COLUMNSPACE2:
+	cmp     eax, 10000
+	jge     COLUMNSPACE3
+	call    WriteString
+
+COLUMNSPACE3:
+	cmp     eax, 1000
+	jge     COLUMNSPACE4
+	call    WriteString
+
+COLUMNSPACE4:
+	cmp     eax, 100
+	jge     COLUMNSPACE5
+	call    WriteString
+
+COLUMNSPACE5:
+	cmp     eax, 10
+	jge     COLUMNSPACE6
+	call    WriteString
+
+COLUMNSPACE6:
+	call    WriteString
+	ret
+WriteColumnSpaces ENDP
+
+;--------------------------------------------------------
+;
+; Entry point for displaying composite numbers
+;
+;--------------------------------------------------------
+
+showComposites PROC
+	mov  ecx, eax ; Move the user value into the loop counter
+	mov  eax, 2
+
+COMPOSITELOOP:
+	push ecx
+	call isComposite ; This function modifies ECX
+	pop  ecx
+	inc eax
+	cmp cNumbersFound, ecx
+	jne COMPOSITELOOP
+
+	ret
+showComposites ENDP
+
+;--------------------------------------------------------
+;
+; A function to print a single number as a composite
+;
+;--------------------------------------------------------
+
+isComposite PROC
+	; EAX contains the number to check as a composite
+	mov    ecx, eax ; Setup loop counter
+	dec    ecx      ; We don't want to divide by it's self, so reduce count by one
+
+	mov    ebx, eax ; EBX will contain the original number
+COMPOSITECHECKLOOP:
+	cmp   ecx, 1    ; 1 is not a composite number
+	je    NOTCOMPOSITE
+
+	mov    eax, ebx ; Restore the original number into eax
+	mov    edx, 0   ; Clear edx
+	div    ecx      ; Divide by the loop counter
+	cmp    edx, 0   ; If there is no remainder, then this is a composite number
+	jne    NOTCOMPOSITE
+
+	mov    edx, cNumbersFound
+	inc    edx
+	mov    cNumbersFound, edx
+
+	mov    eax, ebx ; Restore the original number into eax
+	call   WriteColumnSpaces
+	call   WriteDec
+
+	mov    eax, columnNumber
+	inc    eax
+	mov    columnNumber, eax
+
+	; We have printed 10 numbers, so do a CrLf
+	cmp    eax, 10
+	jne    COMPOSITECOLUMN
+	mov    columnNumber, 0
+	call   CrLf
+
+	; Pause the print out and display message if we have hit the console size
+	
+	mov    eax, 0
+	mov    al, rowNumber
+	inc    al
+	mov    rowNumber, al
+	call   GetMaxXY
+	cmp    rowNumber, dh
+	jne    COMPOSITECOLUMN
+	
+	mov    rowNumber, 0
+	call   PauseOutput
+
+COMPOSITECOLUMN:
+
+	mov    eax, ebx ; Restore the original number into eax
+	ret             ; Exit the procedure now that we found the composite number
+
+NOTCOMPOSITE:
+	loop COMPOSITECHECKLOOP
+	mov    eax, ebx ; Restore the original number into eax
+	ret
+isComposite ENDP
+
+;--------------------------------------------------------
+;
+; Program entry point
+;
+;--------------------------------------------------------
+
+main PROC
+	
+	call displayIntro
+	call getUsername
+	call displayInstructions
+	call getInput
+	call showComposites
+
+	call displayExit
 	exit	; exit to operating system
 main ENDP
 

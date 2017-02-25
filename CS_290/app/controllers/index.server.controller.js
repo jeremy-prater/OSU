ea7d623622bf7b92056e5a30d7aa3e5b05b5fe12
Jeremy Prater <jeremy.prater@trailtech.net>
2017-02-25 00:39:22 +0000

    - CS 290 work.

--- CS_290/app/controllers/index.server.controller.js
+++ CS_290/app/controllers/index.server.controller.js
@@ -2,7 +2,18 @@ const pug = require('pug');
 
 exports.render = function(req, res) {
 
-    //console.log(req);
+    //console.log(req.headers);
+
+    this.stuff = [];
+
+    var items = Object.keys(req.headers);
+    for (var itemIndex=0; itemIndex < items.length; itemIndex++) {
+        var currentItem = req.headers[items[itemIndex]];
+        stuff.push({
+            name: items[itemIndex],
+            value: currentItem
+        });
+    }
 
     if (req.method === 'GET')
     {
@@ -16,6 +27,7 @@ exports.render = function(req, res) {
     res.render('index', {
         title: 'CS_290: Assignment 2 - HTML/CSS',
         message: 'Pug is awesome!',
-        httpMethod: this.httpMethod
+        httpMethod: this.httpMethod,
+        stuff: this.stuff
     })
 };

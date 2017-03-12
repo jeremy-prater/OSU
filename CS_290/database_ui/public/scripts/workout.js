a5bbcf4b1ae7a80d70dc0a657bacdbdac9c3119b
Jeremy Prater <jeremy.prater@trailtech.net>
2017-03-12 11:13:48 -0700

    - CS_290 database_ui work : UI Framework

--- CS_290/database_ui/public/scripts/workout.js
+++ CS_290/database_ui/public/scripts/workout.js
@@ -3,9 +3,80 @@
 // Bootstrap components once the page is loaded
 //
 
-function addWorkout()
-{
-    console.log ("[Workout] Adding workout.");
+var sortType = 0;
+var lastSortType = 0;
+
+////////////////////////////////////////////////////////////
+//
+// Initalize any DOM objects data
+//
+
+
+////////////////////////////////////////////////////////////
+//
+// Click handler functions
+//
+
+function sortDate() {
+    sortType = 0;
+    updateWorkouts();
+}
+
+function sortWeight() {
+    sortType = 1;
+    updateWorkouts();
+}
+
+function sortReps() {
+    sortType = 2;
+    updateWorkouts();
+}
+
+function clearAll() {
+    // Delete local client data...
+    updateWorkouts();
+}
+
+////////////////////////////////////////////////////////////
+//
+// Client DOM update functions
+//
+
+function updateWorkouts() {
+    if (lastSortType == sortType) {
+        return;
+    } else {
+        lastSortType = sortType;
+    }
+
+    console.log("[UPDATE] Workout Sort : " + sortType);
+
+    switch (sortType) {
+        case 0: //sort by date
+            {
+
+            }
+            break;
+        case 1: //sort by weight
+            {
+
+            }
+            break;
+        case 2: //sort by reps
+            {
+
+            }
+            break;
+    }
+}
+
+////////////////////////////////////////////////////////////
+//
+// Database access functions
+//
+
+function addWorkout() {
+    console.log("[Workout] Adding workout.");
     var payload = {
         name: document.getElementById('name').value,
         reps: document.getElementById('reps').value,
@@ -18,9 +89,17 @@ function addWorkout()
     req.open('POST', targetUrl, true);
     req.setRequestHeader("Content-type", "application/json");
     req.onreadystatechange = function () {
-        if (req.status >= 200 && req.status < 400) {
-        }
+        if (req.status >= 200 && req.status < 400) {}
     };
-    console.log(payload);
     req.send(JSON.stringify(payload));
 }
+
+////////////////////////////////////////////////////////////
+//
+// Client startup functions
+//
+
+// Set the Date input to the current DATETIEM (sql)
+document.getElementById('date').value = new Date(Date.now()).toISOString();
+
+updateWorkouts();

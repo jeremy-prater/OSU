f0b2f6d1e1292e0ee4da43fe7016e24a41f69bf1
Jeremy Prater <jeremy.prater@trailtech.net>
2017-07-02 09:56:22 -0700

    - CS 325 hw 1 work

--- CS_325/hw1/src/hw1.cpp
+++ CS_325/hw1/src/hw1.cpp
@@ -11,14 +11,20 @@
 #include <math.h>
 #include <string.h>
 #include "CSVLogger.hpp"
+#include <time.h>
 #include <stdlib.h>
 
 static const char * dataLog = "./hw1.csv";
-static const char * dataSchema = "NUM_ELEMENTS,INSERT_METHOD,RUNTIME";
+static const char * dataSchema = "NUM_ELEMENTS,INSERT_METHOD,RUNTIME,RUNTIMEAVG";
+
+static const uint32_t numIterations = 10;
+
+static struct timespec time0;
+static struct timespec time1;
 
 int main( int argc, char *argv[ ] )
 {
-    if (argc != 4)
+    if (argc != 3)
     {
         printf("Incorrect number of arguments...\n");
         exit(-1);
@@ -33,15 +39,12 @@ int main( int argc, char *argv[ ] )
     const unsigned int NUM_ELEMENTS = atoi (argv[2]);
 
     printf ("-> USE_DATA: %d\t", USE_DATA);
-    printf ("-> Elements: %d\t", NUM_ELEMENTS);
+    printf ("-> ELEMENTS: %d\t", NUM_ELEMENTS);
     CSVLogger::WriteLog("%d, %d, %d, %d, %f, %f",
-    //DEVICE_NAME,
+    USE_DATA,
     NUM_ELEMENTS,
-    LOCAL_SIZE,
-    OP_MODE,
-    NUM_WORK_GROUPS,
-    megaMultsSecBest,
-    megaMultsSecAvg);
+    runTime,
+    runTimeAvg);
 
 
     CSVLogger::CloseLogFile();

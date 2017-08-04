5bad44ac6568100565c1710594d50f03ebdb7572
Jeremy Prater <jeremy.prater@trailtech.net>
2017-08-03 23:34:37 -0700

    - CS 325 HW 6 almost works

--- CS_325/hw6/problem1.py
+++ CS_325/hw6/problem1.py
@@ -2,6 +2,8 @@
 
 import scipy.optimize
 
+totalNodeCount = 0
+
 distances = [
     {'start':'g', 'end':'d', 'distance':2},
     {'start':'g', 'end':'h', 'distance':3},
@@ -26,18 +28,22 @@ def FindDistance (start, end):
     for item in distances:
         if item['start'] == start and item['end'] == end:
             return item['distance']
+    return 0
 
 def FindEnds (startingNodes):
+    global totalNodeCount
     ends = []
     for start in startingNodes:
         for item in distances:
             if item['start'] == start:
                 ends.append({'start' : start, 'end' : item['end']})
+                totalNodeCount += 1
     return ends
 
 def FindMinPath(start, end):
+    global totalNodeCount
+    totalNodeCount = 0
     # find contraints
-    # Generate initial set
     searching = True
     startingNodes = [start]
     nodes = []
@@ -51,15 +57,65 @@ def FindMinPath(start, end):
             if testEnd['end'] == end:
                 searching = False
 
-        nodes.append(newEnds)
+        for node in newEnds:
+            nodes.append(node)
 
-    # compute matrix
-    print (nodes)
+    # generate matricies
+    #print ("total nodes: " + str(totalNodeCount))
+    #print (nodes)
     
-    # solve
+    c = []
+    A = []
+    B = []
 
-    scipy.optimize.linprog()
-    return str(111)
+    for aIndex in range (0, totalNodeCount) :
+        arrayBuilder = []
+        for bIndex in range (0, totalNodeCount) :
+            arrayBuilder.append([])
+        A.append(arrayBuilder)
+
+    index = 0
+    for node in nodes:
+        c.append(1)
+        if node['end'] == end:
+            B.append(-1)
+        elif node['start'] == start:
+            B.append(1)
+        else:
+            B.append(0)
+
+        #print (node)
+        for aIndex in range (0, totalNodeCount) :
+            #print ("---")
+            #print (aIndex)
+            #print (len(nodes))
+            #print (nodes[aIndex])
+            x = nodes[aIndex]['start']
+            y = node['end']
+            A[aIndex][index] = (- FindDistance(x, y) + FindDistance(y, x))
+            
+        index += 1
+
+
+    # solve
+    #print ("c")
+    #print (c)
+    #print ("A")
+    #print (A)
+    #print ("B")
+    #print (B)
+    res = scipy.optimize.linprog(c,A,B)
+
+    # multiply-sum the elements of x with the distances to get the distance
+    index = 0
+    totalDistance = 0
+    for x in res.x:
+        totalDistance += FindDistance(nodes[index]['start'], nodes[index]['end']) * x
+        index += 1
+
+    #print (totalDistance)
+
+    return str(totalDistance)
 
 # find a path from G to all other nodes with the minimum distance
 for destLoop in range(ord('a'), ord('h') + 1):

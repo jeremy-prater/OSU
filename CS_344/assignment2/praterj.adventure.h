2cdca8dd088fc43c07c7b3fcf16ba8629f1807d5
Jeremy Prater <jeremy.prater@trailtech.net>
2017-10-13 00:34:18 -0700

    - CS 344 Assignment 2 Buildrooms

--- CS_344/assignment2/praterj.adventure.h
+++ CS_344/assignment2/praterj.adventure.h
@@ -14,13 +14,24 @@
 #define NUM_DEFAULT_CONNECTIONS 3
 #define MAX_ROOM_PATH 256
 
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+typedef enum
+{
+    ROOM_START,
+    ROOM_MID,
+    ROOM_END
+} ROOM_TYPE;
+
 typedef struct
 {
     const char * roomName;
     char roomFile[MAX_ROOM_PATH];
     struct room_t * roomConnections[NUM_CONNECTIONS];
     int roomCreated;
+    int roomChecked;
     int numConnections;
+    ROOM_TYPE roomType;
 } room_t;
 
 #endif // CS344_ADVENTURE_H
--- CS_344/assignment2/praterj.buildrooms.c
+++ CS_344/assignment2/praterj.buildrooms.c
@@ -27,11 +27,14 @@ static const char * roomNames[MAX_ROOMS] = {
 };
 
 static room_t roomData[NUM_ROOMS];
+static room_t * startRoom = 0;
+static room_t * endRoom = 0;
+static int totalLinks = 0;
 
 void CreateRooms(room_t * rooms)
 {
     srand (time(NULL));
-    memset (roomData, 0, sizeof (rooms));
+    memset (roomData, 0, sizeof (roomData));
     int roomCreationCount = 0;
     while (roomCreationCount < NUM_ROOMS)
     {
@@ -64,9 +67,20 @@ void CreateRooms(room_t * rooms)
             snprintf (rooms[roomCreationCount].roomFile, MAX_ROOM_PATH, "praterj.rooms.%d.%s", getpid(), rooms[roomCreationCount].roomName);
             printf ("Created room [%s] with file name [%s]\n", rooms[roomCreationCount].roomName, rooms[roomCreationCount].roomFile);
             rooms[roomCreationCount].roomCreated = 1;
+            rooms[roomCreationCount].roomType = ROOM_MID;
             roomCreationCount++;
         }
     }
+    int startRoomIndex = rand() % NUM_ROOMS;
+    int endRoomIndex = startRoomIndex;
+    while (endRoomIndex == startRoomIndex)
+    {
+        endRoomIndex = rand() % NUM_ROOMS;
+    }
+    startRoom = &rooms[startRoomIndex];
+    endRoom = &rooms[endRoomIndex];
+    startRoom->roomType = ROOM_START;
+    endRoom->roomType = ROOM_END;
 }
 
 int FindEmptyRoomSlot(room_t * room)
@@ -83,61 +97,153 @@ int FindEmptyRoomSlot(room_t * room)
 
 int CreateLink(room_t * rooms, int A, int B)
 {
-    for (int testRoomIndex = 0; testRoomIndex < NUM_ROOMS; testRoomIndex++)
+    if (A == B)
     {
-        // Check if room B is already connected to room A
-        if (rooms[B].roomConnections[testRoomIndex] == (struct room_t*)&rooms[A])
+        return 0;
+    }
+
+    for (int testRoomIndexA = 0; testRoomIndexA < NUM_CONNECTIONS; testRoomIndexA++)
+    {
+        for (int testRoomIndexB = 0; testRoomIndexB < NUM_CONNECTIONS; testRoomIndexB++)
         {
-            // No good, the rooms are already connected...
-            //printf ("[%s] is already connected to [%s]\n", rooms[A].roomName, rooms[B].roomName);
-            return 0;
+            // Check if room B is already connected to room A
+            if (
+                (rooms[A].roomConnections[testRoomIndexA]) &&
+                (rooms[B].roomConnections[testRoomIndexB]) &&
+                (rooms[A].roomConnections[testRoomIndexA] == rooms[B].roomConnections[testRoomIndexB])
+                )
+            {
+                // No good, the rooms are already connected...
+                /*printf ("[%s] is already connected to [%s] [%p]==[%p]\n",
+                    rooms[A].roomName,
+                    rooms[B].roomName,
+                    rooms[A].roomConnections[testRoomIndexA],
+                    rooms[B].roomConnections[testRoomIndexB]);*/
+                return 0;
+            }
         }
     }
-    printf ("Connecting [%s] to [%s]\n", rooms[A].roomName, rooms[B].roomName);
     // Find empty slot and connect rooms
     int aIndex = FindEmptyRoomSlot(&rooms[A]);
     int bIndex = FindEmptyRoomSlot(&rooms[B]);
     rooms[A].roomConnections[aIndex] = (struct room_t*)&rooms[B];
     rooms[B].roomConnections[bIndex] = (struct room_t*)&rooms[A];
+    rooms[A].numConnections++;
+    rooms[B].numConnections++;
+    printf ("Connecting [%s] to [%s] [%p]=[%p]\n",
+        rooms[A].roomName,
+        rooms[B].roomName,
+        rooms[A].roomConnections[aIndex],
+        rooms[B].roomConnections[bIndex]);
+
+    totalLinks++;
     return 1;
 }
 
 void LinkRooms(room_t * rooms)
 {
-    // Starting room is at index 0
-    // Ending room is at NUM_ROOMS
-    // A tree is probably the best data structure to do this
-    // The ending room will be a leaf at the outermost branch
-    for (int roomConnectionCount = 0; roomConnectionCount < NUM_DEFAULT_CONNECTIONS; roomConnectionCount++)
+    int linking = 1;
+    while (linking)
     {
+        int minLinks = NUM_ROOMS;
         for (int roomIndex = 0; roomIndex < NUM_ROOMS; roomIndex++)
         {
             // Generate a connecting room index
             int A = roomIndex;
             int B = -1;
             int searching = 1;
-            while (searching)
+            B = rand() % NUM_ROOMS;
+            if (B != A)
             {
-                B = rand() % NUM_ROOMS;
-                if (B != A)
+                // No rooms connect to self
+                if (CreateLink(rooms, A, B))
                 {
-                    // No rooms connect to self
-                    searching = !CreateLink(rooms, A, B);
+                    minLinks = MIN(minLinks, rooms[A].numConnections);
+                    minLinks = MIN(minLinks, rooms[B].numConnections);
                 }
             }
+        }
+        if (minLinks >= 3)
+        {
+            linking = 0;
         }
     }
 }
 
+int CheckConnectivity(room_t * start, room_t * end)
+{
+    // Recurisive functions! Yay!
+    start->roomChecked = 1;
+    if (start == end)
+    {
+        return 1;
+    }
+
+    int foundIt = 0;
+    for (int roomIndex = 0; roomIndex < NUM_CONNECTIONS; roomIndex++)
+    {
+        if (!(room_t*)start->roomConnections[roomIndex])
+        {
+            continue;
+        }
+
+        if (!((room_t*)start->roomConnections[roomIndex])->roomChecked)
+        {
+            foundIt |= CheckConnectivity((room_t*)start->roomConnections[roomIndex], end);
+        }
+    }
+    return foundIt;
+}
+
+void SaveRoom(room_t * room)
+{
+    printf ("Saving file [%s]\n", room->roomFile);
+    FILE * outFile = fopen(room->roomFile, "w");
+    fprintf (outFile, "ROOM NAME: %s\n", room->roomName);
+    
+    for (int connectionIndex = 0; connectionIndex < NUM_CONNECTIONS; connectionIndex++)
+    {
+        if (room->roomConnections[connectionIndex])
+        {
+            fprintf (outFile, "CONNECTION %d: %s\n", connectionIndex + 1, ((room_t*)room->roomConnections[connectionIndex])->roomName);
+        }
+    }
+
+    switch (room->roomType)
+    {
+        case ROOM_START:
+        fprintf (outFile, "ROOM TYPE: START_ROOM\n");
+        break;
+        case ROOM_MID:
+        fprintf (outFile, "ROOM TYPE: MID_ROOM\n");
+        break;
+        case ROOM_END:
+        fprintf (outFile, "ROOM TYPE: END_ROOM\n");
+        break;
+    }
+    
+    fclose(outFile);
+}
+
 void SaveRooms(room_t * rooms)
 {
-
+    for (int roomIndex = 0; roomIndex < NUM_ROOMS; roomIndex++)
+    {
+        SaveRoom (&rooms[roomIndex]);
+    }
 }
 
 int main(int argc, char * argv[])
 {
+    int winning = 1;
     CreateRooms(roomData);
     LinkRooms (roomData);
+    while (!CheckConnectivity(startRoom, endRoom))
+    {
+        CreateLink(roomData, rand() % NUM_ROOMS, rand() % NUM_ROOMS);
+    }
+    printf ("Total room connections [%d]\n", totalLinks);
     SaveRooms (roomData);
+
     return 0;
 }

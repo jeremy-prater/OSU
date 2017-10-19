86e348dbf67e35ca492f3cd2bbb9c14323a10e3a
Jeremy Prater <jeremy.prater@trailtech.net>
2017-10-19 01:13:04 -0700

    - CS 344 asventure game work

--- CS_344/assignment2/praterj.adventure.c
+++ CS_344/assignment2/praterj.adventure.c
@@ -13,9 +13,52 @@
 #include <stdlib.h>
 #include <time.h>
 #include "praterj.adventure.h"
+#include <dirent.h>
+
+char gameDirectory[64];
+
+int CheckTimeDiff (const struct timespec lhs, const struct timespec rhs)
+{
+    if (lhs.tv_sec == rhs.tv_sec)
+        return lhs.tv_nsec > rhs.tv_nsec;
+    else
+        return lhs.tv_sec > rhs.tv_sec;
+}
+
+char * FindNewestGameDir()
+{
+    DIR * directoryScanner = opendir(".");
+    struct dirent * currentDirectory;
+    struct timespec newestDirectory;
+    memset (&newestDirectory, 0, sizeof (newestDirectory));
+
+    while ((currentDirectory = readdir (directoryScanner)))
+    {
+        if ((currentDirectory->d_type == DT_DIR) && (strstr(currentDirectory->d_name, "praterj.rooms") != NULL))
+        {
+            struct stat dirStat;
+            stat (currentDirectory->d_name, &dirStat);
+            if (CheckTimeDiff(dirStat.st_mtim, newestDirectory))
+            {
+                strcpy(gameDirectory, currentDirectory->d_name);
+                memcpy (&newestDirectory, &dirStat.st_mtim, sizeof (struct timespec));
+            }
+        }
+    }
+    printf ("Using game directory [%s]\n", gameDirectory);
+    closedir (directoryScanner);
+}
+
+
 
 int main(int argc, char * argv[])
 {
-
+    memset (gameDirectory, 0, sizeof (gameDirectory));
+    FindNewestGameDir();
+    if (strlen(gameDirectory) == 0)
+    {
+        printf("No valid game directories found! run praterj.buildsrooms first\n");
+        return -1;
+    }
     return 0;
 }

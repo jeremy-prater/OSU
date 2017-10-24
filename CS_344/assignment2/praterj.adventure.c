2fb63aee4594e8ca3ce1f6019ef69149440e7566
Jeremy Prater <jeremy.prater@trailtech.net>
2017-10-23 19:54:12 -0700

    - CS 344 Assignment 2 comments

--- CS_344/assignment2/praterj.adventure.c
+++ CS_344/assignment2/praterj.adventure.c
@@ -13,9 +13,58 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
-#include "praterj.adventure.h"
 #include <dirent.h>
 #include <pthread.h>
+
+///////////////////////////////////////////////////////
+//
+// Definitions for program configuration
+//
+
+#define NUM_ROOMS 7
+#define MAX_ROOMS 10
+#define NUM_CONNECTIONS 6
+#define NUM_DEFAULT_CONNECTIONS 3
+#define MAX_ROOM_PATH 256
+
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+///////////////////////////////////////////////////////
+//
+// Enumerations
+//
+
+typedef enum
+{
+    ROOM_START = 1,
+    ROOM_MID = 2,
+    ROOM_END = 3
+} ROOM_TYPE;
+
+///////////////////////////////////////////////////////
+//
+// Room data structure
+//
+
+typedef struct
+{
+    const char * roomName;
+    char roomFile[MAX_ROOM_PATH];
+    struct room_t * roomConnections[NUM_CONNECTIONS];
+    const char * roomConnectionIDs[NUM_CONNECTIONS];
+    int roomCreated;
+    int roomChecked;
+    int numConnections;
+    ROOM_TYPE roomType;
+} room_t;
+
+///////////////////////////////////////////////////////
+//
+// Global variables...
+//
+// This should be avoided, but given the scope of the
+// scope of the program, this is the easiest way.
+//
 
 char gameDirectory[64];
 room_t rooms[NUM_ROOMS];
@@ -27,6 +76,12 @@ pthread_mutex_t timeMutex = PTHREAD_MUTEX_INITIALIZER;
 int timeThreadRunning = 1;
 volatile int timerRunning = 0;
 
+///////////////////////////////////////////////////////
+//
+// CheckTimeDiff returns true if the lhs time is
+// greater than the rhs time, or false other wise.
+//
+
 int CheckTimeDiff (const struct timespec lhs, const struct timespec rhs)
 {
     if (lhs.tv_sec == rhs.tv_sec)
@@ -35,21 +90,32 @@ int CheckTimeDiff (const struct timespec lhs, const struct timespec rhs)
         return lhs.tv_sec > rhs.tv_sec;
 }
 
+///////////////////////////////////////////////////////
+//
+// This returns the string of the newest directory
+// that contains "pratej.rooms" based on st_mtim
+//
+
 char * FindNewestGameDir()
 {
+    // Open the current directory for reading all entries
     DIR * directoryScanner = opendir(".");
     struct dirent * currentDirectory;
     struct timespec newestDirectory;
     memset (&newestDirectory, 0, sizeof (newestDirectory));
 
+    // Iterate while there while there are more entries
     while ((currentDirectory = readdir (directoryScanner)))
     {
+        // Check if the entry is a directory and contains "praterj.rooms"
         if ((currentDirectory->d_type == DT_DIR) && (strstr(currentDirectory->d_name, "praterj.rooms") != NULL))
         {
             struct stat dirStat;
             stat (currentDirectory->d_name, &dirStat);
+            // Check the time of the directory
             if (CheckTimeDiff(dirStat.st_mtim, newestDirectory))
             {
+                // Save the newest directory
                 strcpy(gameDirectory, currentDirectory->d_name);
                 memcpy (&newestDirectory, &dirStat.st_mtim, sizeof (struct timespec));
             }
@@ -59,24 +125,36 @@ char * FindNewestGameDir()
     closedir (directoryScanner);
 }
 
+///////////////////////////////////////////////////////
+//
+// This Loads a room into the room[] array based on
+// idex and a filename to load
+//
+
 void LoadRoom(int roomIndex, char * roomFileName)
 {
     char localFileName[128];
+    // Prepend the directory on the room filename
     snprintf(localFileName, 128, "%s/%s", gameDirectory, roomFileName);
     char localBuffer[128];
     printf ("Loading room [%s]\n", localFileName);
+    // Open file for reading
     FILE * roomFile = fopen(localFileName, "r");
     if (roomFile == NULL)
     {
         printf ("Error Loading room [%s] [%s]\n", roomFileName, strerror(errno));
         exit(-2);
     }
+    // Get the first line
     fgets(localBuffer, 128, roomFile);
+
+    // Main line parsing loop
     do
     {
         localBuffer[strlen(localBuffer) - 1] = 0x00;
         if (strstr(localBuffer, "ROOM NAME"))
         {
+            // The line is the ROOM NAME, store it in the destination
             char * token = strtok (localBuffer, ":");
             token = strtok (NULL, ":");
             token++;
@@ -86,6 +164,7 @@ void LoadRoom(int roomIndex, char * roomFileName)
 
         if (strstr(localBuffer, "CONNECTION"))
         {
+            // The line is a connecion, store it in the destination
             char * token = strtok (localBuffer, " ");
             token = strtok (NULL, " ");
             token[1] = 0x00;
@@ -98,9 +177,12 @@ void LoadRoom(int roomIndex, char * roomFileName)
 
         if (strstr(localBuffer, "ROOM TYPE"))
         {
+            // The line is the ROOM TYPE, store it in the destination
             char * token = strtok (localBuffer, ":");
             token = strtok (NULL, ":");
             token++;
+            // Convert from a string into an enum.
+            // Store the start and end rooms in global pointers
             if (strstr(token, "START_ROOM"))
             {
                 rooms[roomIndex].roomType = ROOM_START;
@@ -120,24 +202,39 @@ void LoadRoom(int roomIndex, char * roomFileName)
         }
 
         fgets(localBuffer, 128, roomFile);
+    // Iterate through all liness
     } while (!feof(roomFile));
     fclose(roomFile);
 }
 
+///////////////////////////////////////////////////////
+//
+// Function to iterate through all room files.
+//
+
 void LoadGameData()
 {
+    // Open the game directory
     DIR * directoryScanner = opendir(gameDirectory);
     struct dirent * currentDirectory;
     int roomIndex = 0;
+    // Loop through all items in the game directory
     while ((currentDirectory = readdir (directoryScanner)))
     {
+        // If the file contains ".room" then load it.
         if (strstr(currentDirectory->d_name, ".room") != NULL)
         {
+            // Load room
             LoadRoom (roomIndex++, currentDirectory->d_name);
         }
     }
     closedir (directoryScanner);
 }
+
+///////////////////////////////////////////////////////
+//
+// Find a pointer to a room based the string name
+//
 
 room_t * FindRoom(const char * name)
 {
@@ -152,18 +249,31 @@ room_t * FindRoom(const char * name)
     return NULL;    
 }
 
+///////////////////////////////////////////////////////
+//
+// Rooms are loaded and have strings that specify
+// connections. Caching pointers to connections
+// instead of string look ups will be easier to manage
+//
+
 void LinkRooms()
 {
     int roomIndex = 0;
+    // Loop through all rooms
     for (roomIndex = 0; roomIndex < NUM_ROOMS; roomIndex++)
     {
         room_t * currentRoom = &rooms[roomIndex];
         int connectionIndex = 0;
+        // Loop through all connections in current room
         for (connectionIndex = 0; connectionIndex < NUM_CONNECTIONS; connectionIndex++)
         {
+            // Check if there is a string in the current connection
             if (rooms[roomIndex].roomConnectionIDs[connectionIndex])
             {
+                // Assign a pointer to the room in the look up array.
                 rooms[roomIndex].roomConnections[connectionIndex] = (struct room_t *)FindRoom(rooms[roomIndex].roomConnectionIDs[connectionIndex]);
+                
+                // This all just debugging to help the user see which rooms are start/end
                 printf ("Linking [%s]->[%s]", rooms[roomIndex].roomName, ((room_t *)rooms[roomIndex].roomConnections[connectionIndex])->roomName);
                 if (rooms[roomIndex].roomType == ROOM_START)
                 {
@@ -179,8 +289,14 @@ void LinkRooms()
     }
 }
 
+///////////////////////////////////////////////////////
+//
+// This is the game running loop
+//
+
 int runGame()
 {
+    // Print current location
     printf ("CURRENT LOCATION: %s\n", currentRoom->roomName);
 
     char roomConnections[512];
@@ -188,10 +304,16 @@ int runGame()
     memset (roomConnections, 512, 0);
     char * roomConnectionStringPtr = roomConnections;
     int connectionIndex = 0;
+    // Loop through all connections
     for (connectionIndex = 0; connectionIndex < NUM_CONNECTIONS; connectionIndex++)
     {
+        // Check if the connection is a pointer to another room
         if (currentRoom->roomConnections[connectionIndex])
         {
+            // Copy the rooms name to the current string.
+            // memcpy was a better choice than strcpy because I do not
+            // want to append the terminating null character now.
+            // This also adds comma ',' and spaces ' '
             const char * connectorName = ((room_t *)currentRoom->roomConnections[connectionIndex])->roomName;
             int stringLength = strlen(connectorName);
             memcpy (roomConnectionStringPtr, connectorName, stringLength);
@@ -202,34 +324,52 @@ int runGame()
             roomConnectionStringPtr++;
         }
     }
+    // Back up two characters
     roomConnectionStringPtr--;
     roomConnectionStringPtr--;
+    // Terminate the string
     *roomConnectionStringPtr = '.';
     roomConnectionStringPtr++;
     *roomConnectionStringPtr = 0x00;
+    // Print the connections
     printf ("POSSIBLE CONNECTIONS: %s\n", roomConnections);
     printf ("WHERE TO? >");
+
+    // Get user input
     fgets(userInput, 128, stdin);
     userInput[strlen(userInput) - 1] = 0x00;
 
     int foundRoom = 0;
+
+    // Check if we are executing the time command
     if (strcmp (userInput, "time") == 0)
     {
-        pthread_mutex_unlock (&timeMutex);
+        // Set the timer running flag
         timerRunning = 1;
+        // Unlock the mutex
+        pthread_mutex_unlock (&timeMutex);
+
+        // Wait here until a context switch occurs into the timer thread
+        // This works becuase timerRunning is a volatile variable
         while (timerRunning);
+
+        // Lock timer mutex
         pthread_mutex_lock (&timeMutex);
         return 0;
     }
     else
     {
+        // We are going to a room.
         int connectionIndex = 0;
+        // Iterate through all connections
         for (connectionIndex = 0; connectionIndex < NUM_CONNECTIONS; connectionIndex++)
         {
             if (currentRoom->roomConnections[connectionIndex])
             {
                 if (strcmp (userInput, ((room_t *)currentRoom->roomConnections[connectionIndex])->roomName) == 0)
                 {
+                    // We found a connection to a new room.
+                    // Move to that room
                     currentRoom = (room_t *)currentRoom->roomConnections[connectionIndex];
                     foundRoom = 1;
                 }
@@ -237,6 +377,7 @@ int runGame()
         }
     }
 
+    // No room was found with that name, so print the error message.
     if (!foundRoom)
     {
         printf ("\nHUH? I DON’T UNDERSTAND THAT ROOM. TRY AGAIN.\n");
@@ -248,13 +389,30 @@ int runGame()
     return 1;
 }
 
+///////////////////////////////////////////////////////
+//
+// This is the timer running loop
+//
+
 void * timeFunction(void * context)
 {
+    // Time thread runs while the main thread is running
     while (timeThreadRunning)
     {
+        // We wait for the mutex to unlock
         pthread_mutex_lock (&timeMutex);
+
+        // Check if the timer is still alive.
+        // This is the exit case, where we set timerThreadRunning to false
+        // and unlock the mutex, which kills the thread
+        // timerRunning is a flag to control what the timer is doing
+        // timeMutex protects access to the timerRunning variable and controls
+        // thread execution.
+
         if (timeThreadRunning && timerRunning)
         {
+            // Print the time and save it to a file called currentTime.txt
+            // timerRunning allows the main thread to continue when the mutex is unlocked.
             timerRunning = 0;
             time_t now;
             time (&now);
@@ -274,26 +432,40 @@ void * timeFunction(void * context)
             fclose (timeFile);
           
         }
+        // Unlock the mutex and return to main thread.
         pthread_mutex_unlock (&timeMutex);
     }
 }
 
+///////////////////////////////////////////////////////
+//
+// Main program entry point
+//
+
 int main(int argc, char * argv[])
 {
+    // Create the timer thread
+    // Lock the timer mutex when program execution begins
     pthread_mutex_lock (&timeMutex);
     if (pthread_create(&timeThread, NULL, timeFunction, NULL))
     {
         printf ("Failed to create time worker thread!\n");
         exit (-3);
     }
+    // Initalize program memory
     memset (gameDirectory, 0, sizeof (gameDirectory));
     memset (rooms, 0, sizeof(room_t) * NUM_ROOMS);
+    
+    // Find the game directory
     FindNewestGameDir();
     if (strlen(gameDirectory) == 0)
     {
+        // Print an error and exit if no rooms are found
         printf("No valid game directories found! run praterj.buildsrooms first\n");
         return -1;
     }
+    
+    // Load game rooms and create pointer links
     LoadGameData();
     LinkRooms();
     printf ("\n");
@@ -307,10 +479,14 @@ int main(int argc, char * argv[])
     printf ("------------------------------------------------------------------------\n");
     printf ("\n");
     
+    // Setup the history array and turn counter
     int turnCounter = 0;
     char ** history = (char**)malloc(sizeof (char*));
+    
+    // Main game loop
     while (currentRoom != endRoom)
     {
+        // Get the status of the turn. true means the turn was successful (ie. a move occured)
         int turnStatus = runGame();
         if (turnStatus)
         {
@@ -319,6 +495,10 @@ int main(int argc, char * argv[])
         }
         turnCounter += turnStatus;
     }
+
+    // The game is over because the current room is the end room
+    
+    // Print game stats and history
     printf ("YOU HAVE FOUND THE END ROOM. CONGRATULATIONS!\n");
     printf ("YOU TOOK %d STEPS. YOUR PATH TO VICTORY WAS:\n", turnCounter);
     int historyIndex = 0;
@@ -328,6 +508,7 @@ int main(int argc, char * argv[])
         free (history[historyIndex]);
     }
 
+    // Clean up any dyamically allocated memory
     free (history);
     int roomIndex = 0;
     for (roomIndex = 0; roomIndex < NUM_ROOMS; roomIndex++)
@@ -343,8 +524,11 @@ int main(int argc, char * argv[])
         }
     }
     
+    // Shutdown the timer thread
     timeThreadRunning = 0;
     pthread_mutex_unlock (&timeMutex);
     pthread_join(timeThread, NULL);
+    
+    // We're outta here!
     return 0;
 }
--- CS_344/assignment2/praterj.buildrooms.c
+++ CS_344/assignment2/praterj.buildrooms.c
@@ -12,7 +12,55 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
-#include "praterj.adventure.h"
+///////////////////////////////////////////////////////
+//
+// Definitions for program configuration
+//
+
+#define NUM_ROOMS 7
+#define MAX_ROOMS 10
+#define NUM_CONNECTIONS 6
+#define NUM_DEFAULT_CONNECTIONS 3
+#define MAX_ROOM_PATH 256
+
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+///////////////////////////////////////////////////////
+//
+// Enumerations
+//
+
+typedef enum
+{
+    ROOM_START = 1,
+    ROOM_MID = 2,
+    ROOM_END = 3
+} ROOM_TYPE;
+
+///////////////////////////////////////////////////////
+//
+// Room data structure
+//
+
+typedef struct
+{
+    const char * roomName;
+    char roomFile[MAX_ROOM_PATH];
+    struct room_t * roomConnections[NUM_CONNECTIONS];
+    const char * roomConnectionIDs[NUM_CONNECTIONS];
+    int roomCreated;
+    int roomChecked;
+    int numConnections;
+    ROOM_TYPE roomType;
+} room_t;
+
+///////////////////////////////////////////////////////
+//
+// Global variables...
+//
+// This should be avoided, but given the scope of the
+// scope of the program, this is the easiest way.
+//
 
 static const char * roomNames[MAX_ROOMS] = {
     "Great Hall",
@@ -32,13 +80,26 @@ static room_t * startRoom = 0;
 static room_t * endRoom = 0;
 static int totalLinks = 0;
 
+///////////////////////////////////////////////////////
+//
+// Create room names, in a random order
+//
+// A room may be created only once
+//
+
 void CreateRooms(room_t * rooms)
 {
+    // Seed the PRNG with the system time
     srand (time(NULL));
+
+    // Initalize room data
     memset (roomData, 0, sizeof (roomData));
+    
+    // Iterate until the correct number of rooms have been created
     int roomCreationCount = 0;
     while (roomCreationCount < NUM_ROOMS)
     {
+        // Find a new room index
         int newRoomIndex = -1;
         while (newRoomIndex == -1)
         {
@@ -59,12 +120,14 @@ void CreateRooms(room_t * rooms)
             }
         }
 
+        // Check if that room has already been created (this should be redundant)
         if (rooms[roomCreationCount].roomCreated)
         {
             continue;
         }
         else
         {
+            // Assign room data to the structure in memory
             rooms[roomCreationCount].roomName = roomNames[newRoomIndex];
             snprintf (rooms[roomCreationCount].roomFile, MAX_ROOM_PATH, "%s.room", rooms[roomCreationCount].roomName);
             printf ("Created room [%s] with file name [%s]\n", rooms[roomCreationCount].roomName, rooms[roomCreationCount].roomFile);
@@ -73,17 +136,27 @@ void CreateRooms(room_t * rooms)
             roomCreationCount++;
         }
     }
+    // Pick the start and end rooms
     int startRoomIndex = rand() % NUM_ROOMS;
     int endRoomIndex = startRoomIndex;
+
+    // Ensure that the start and end rooms are not equal
     while (endRoomIndex == startRoomIndex)
     {
         endRoomIndex = rand() % NUM_ROOMS;
     }
+
+    // Final setup
     startRoom = &rooms[startRoomIndex];
     endRoom = &rooms[endRoomIndex];
     startRoom->roomType = ROOM_START;
     endRoom->roomType = ROOM_END;
 }
+
+///////////////////////////////////////////////////////
+//
+// Find the next empty connection slot for a given room
+//
 
 int FindEmptyRoomSlot(room_t * room)
 {
@@ -98,12 +171,23 @@ int FindEmptyRoomSlot(room_t * room)
     return -1;
 }
 
+///////////////////////////////////////////////////////
+//
+// Attempt to create a link between too rooms by index
+//
+// Returns 1 for success
+// Returns 0 for failed to create link
+//
+
 int CreateLink(room_t * rooms, int A, int B)
 {
+    // If rooms are equal, fail
     if (A == B)
     {
         return 0;
     }
+
+    // Iterate through all connections in A and B to check if they are already connected
     int testRoomIndexA = 0;
     for (testRoomIndexA = 0; testRoomIndexA < NUM_CONNECTIONS; testRoomIndexA++)
     {
@@ -120,11 +204,6 @@ int CreateLink(room_t * rooms, int A, int B)
                 ))
             {
                 // No good, the rooms are already connected...
-                /*printf ("[%s] is already connected to [%s] [%p]==[%p]\n",
-                    rooms[A].roomName,
-                    rooms[B].roomName,
-                    rooms[A].roomConnections[testRoomIndexA],
-                    rooms[B].roomConnections[testRoomIndexB]);*/
                 return 0;
             }
         }
@@ -142,17 +221,29 @@ int CreateLink(room_t * rooms, int A, int B)
         rooms[A].roomConnections[aIndex],
         rooms[B].roomConnections[bIndex]);
 
+    // Increment the total link counter
     totalLinks++;
     return 1;
 }
 
+///////////////////////////////////////////////////////
+//
+// Link random rooms together
+//
+// All rooms must have at LEAST 3 connections
+// No duplicate connections are allowed
+//
+
 void LinkRooms(room_t * rooms)
 {
+    // status variable to determine if we have met exit conditions
     int linking = 1;
     while (linking)
     {
+        // minLinks is the variable that is the MINIMUM number of links for all rooms this round
         int minLinks = NUM_ROOMS;
         int roomIndex = 0;
+        // Iterate through all rooms
         for (roomIndex = 0; roomIndex < NUM_ROOMS; roomIndex++)
         {
             // Generate a connecting room index
@@ -165,11 +256,13 @@ void LinkRooms(room_t * rooms)
                 // No rooms connect to self
                 if (CreateLink(rooms, A, B))
                 {
+                    // If the link was successful then get the minimum number of connections
                     minLinks = MIN(minLinks, rooms[A].numConnections);
                     minLinks = MIN(minLinks, rooms[B].numConnections);
                 }
             }
         }
+        // If we have 3 or more connections for ALL rooms, then exit.
         if (minLinks >= 3)
         {
             linking = 0;
@@ -177,38 +270,67 @@ void LinkRooms(room_t * rooms)
     }
 }
 
+///////////////////////////////////////////////////////
+//
+// Check that there is a path from start->end
+//
+// This is a recursive depth-first search
+//
+
 int CheckConnectivity(room_t * start, room_t * end)
 {
     // Recurisive functions! Yay!
+
+    // Flag that we have already checked this path
+    // There is a posiblity that a circular graph could be created
+    // This causes an infinite loop
     start->roomChecked = 1;
+    
     if (start == end)
     {
+        // We found the end!
         return 1;
     }
 
+    // Loop over all room connections
     int foundIt = 0;
     int roomIndex = 0;
     for (roomIndex = 0; roomIndex < NUM_CONNECTIONS; roomIndex++)
     {
+        // If there is no connection in this index, then skip it
         if (!(room_t*)start->roomConnections[roomIndex])
         {
             continue;
         }
 
+        // If the connecting room has not yet been checked...
         if (!((room_t*)start->roomConnections[roomIndex])->roomChecked)
         {
+            // Logically OR the result from this function with all other connections
+            // in this room, recursively...
             foundIt |= CheckConnectivity((room_t*)start->roomConnections[roomIndex], end);
         }
     }
+
+    // Return the recursive search result
     return foundIt;
 }
 
+///////////////////////////////////////////////////////
+//
+// Save an individual room to the filesystem
+//
+
 void SaveRoom(room_t * room)
 {
+    // Open the output file
     printf ("Saving file [%s]\n", room->roomFile);
     FILE * outFile = fopen(room->roomFile, "w");
+    
+    // Write the ROOM NAME
     fprintf (outFile, "ROOM NAME: %s\n", room->roomName);
     
+    // Write all connections
     int connectionIndex = 0;
     for (connectionIndex = 0; connectionIndex < NUM_CONNECTIONS; connectionIndex++)
     {
@@ -218,6 +340,7 @@ void SaveRoom(room_t * room)
         }
     }
 
+    // Write the ROOM TYPE
     switch (room->roomType)
     {
         case ROOM_START:
@@ -231,34 +354,64 @@ void SaveRoom(room_t * room)
         break;
     }
     
+    // Close the file
     fclose(outFile);
 }
 
+///////////////////////////////////////////////////////
+//
+// Save room data to the filesystem
+//
+
 void SaveRooms(room_t * rooms)
 {
+    // Create the output directory
     char directory[64];
     snprintf (directory, MAX_ROOM_PATH, "./praterj.rooms.%d", getpid());
     mkdir(directory, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    
+    // Enter the output directory
     chdir (directory);
+
+    // Loop over each room and save it to the drive
     int roomIndex = 0;
     for (roomIndex = 0; roomIndex < NUM_ROOMS; roomIndex++)
     {
         SaveRoom (&rooms[roomIndex]);
     }
+    
+    // Return to the parent directory
     chdir ("..");
     printf ("Game data saved in [%s]\n", directory);
 }
 
+///////////////////////////////////////////////////////
+//
+// Main program entry point
+//
+
 int main(int argc, char * argv[])
 {
+    // Create the array of room names
     CreateRooms(roomData);
+
+    // Randomly link rooms
     LinkRooms (roomData);
+
+    // Check that there is a path from start->end
     while (!CheckConnectivity(startRoom, endRoom))
     {
+        // Randomly add a link in hopes that a path is created
+        // from start->end
         CreateLink(roomData, rand() % NUM_ROOMS, rand() % NUM_ROOMS);
     }
+
+    // Print the total number of room connections
     printf ("Total room connections [%d]\n", totalLinks);
+    
+    // Save the room data to the filesystem.
     SaveRooms (roomData);
 
+    // We're outta here!
     return 0;
 }

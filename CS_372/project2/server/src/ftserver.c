eeb93607c3502d689fb042cb451ee34cd8a3d32d
Jeremy Prater <jeremy.prater@trailtech.net>
2017-11-14 23:27:30 -0800

    - CS_372 Project 2 inital work

---
+++ CS_372/project2/server/src/ftserver.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <errno.h>
+#include <string.h>
+
+static int serverPort = -1;
+static int serverSocket = -1;
+static int serverConnection = -1;
+
+int main(int argc, char * argv[])
+{
+    if (argc != 2)
+    {
+        printf("Invald arguments!\n\n%s <server port>\n", argv[0]);
+        return -1;
+    }
+
+    if ((sscanf (argv[1], "%d", &serverPort) != 1) || (serverPort < 0) || (serverPort > 0xFFFF))
+    {
+        printf("Invalid port!! Must be 0-65535 [%d]\n\n", serverPort);
+        return -1;       
+    }
+
+    // Create TCP socket on port
+    if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        printf("Failed to create TCP server socket [%s]\n\n", strerror(errno));
+        return -errno;
+    }
+
+    struct sockaddr_in ftServerSock = {0};
+    ftServerSock.sin_family = AF_INET;
+    ftServerSock.sin_addr.s_addr = INADDR_ANY;
+    ftServerSock.sin_port = htons (serverPort);
+
+    // Bind to port
+    if (bind(serverSocket, (struct sockaddr*)&ftServerSock, sizeof (ftServerSock)) < 0)
+    {
+        printf("Failed to bind TCP server socket [%s]\n\n", strerror(errno));
+        return -errno;
+    }
+
+    if (listen(serverSocket, 1) < 0)
+    {
+        printf("Failed to listen on TCP server socket [%s]\n\n", strerror(errno));
+        return -errno;
+    }
+
+    while ((serverConnection = accept (serverSocket, (struct sockaddr*)&ftServerSock, (socklen_t*)sizeof (ftServerSock))) < 0)
+    {
+        printf ("New Connection! [%d]\n", serverConnection);
+    }
+}

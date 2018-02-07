6d1447882442407cea4036dacd8dc3dcd2d9bffd
Jeremy Prater <JPrate@polarisind.com>
2018-02-06 22:00:57 -0800

    - CS 496 Begin OAuth2 code

--- CS_496/assignment3/server/config/express.js
+++ CS_496/assignment3/server/config/express.js
@@ -10,6 +10,7 @@ module.exports = function() {
     app.use('/css',       express.static('css'));
     app.use('/bootstrap', express.static('bootstrap'));
     app.use('/assets',    express.static('assets'));
+    app.use('/client',    express.static('client'));
 
     require('../app/routes/main.server.routes.js')(app);
     return app;

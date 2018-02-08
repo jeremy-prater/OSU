cfe8a4f139c5c0d288143c1894c6320a55ceb98e
Jeremy Prater <silverevo03@gmail.com>
2018-02-07 23:48:29 -0800

    - CS 496 - Add access token to client header

--- CS_496/assignment3/server/client/getUserInfo.js
+++ CS_496/assignment3/server/client/getUserInfo.js
@@ -5,16 +5,18 @@ function UserInfo() {
     this.userInfoURL = "www.googleapis.com/plus/v1/people/me"
 
     this.Connect = function(tokenObject) {
-        http.open("GET", this.userInfoURL);
-        http.onreadystatechange = function() {//Call a function when the state changes.
-            if(http.readyState == 4 && http.status == 200) {
+        var req = new XMLHttpRequest();
+        req.open('GET', userInfoURL, true);
+        req.setRequestHeader("Authorization", `${tokenObject.token_type} ${tokenObject.access_token}`);
+        req.onreadystatechange = function () {
+            if (req.status == 200 && req.readyState === 4) {
                 console.log(http.responseText);
                 // Update DOM elements!
             } else {
                 console.log(`Google API request failed [${http.status}] [${http.responseText}]`);
             }
-        }
-        http.send(null);
+        };
+        req.send(null);
     }
 
     return this;

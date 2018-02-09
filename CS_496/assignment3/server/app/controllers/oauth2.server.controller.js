bacf5d54fb23b6c59bb73ce5d7fc3e0d501fc666
Jeremy Prater <silverevo03@gmail.com>
2018-02-09 00:06:38 -0800

    - CS 496 Assignment 3 - Final work... I thinkk

--- CS_496/assignment3/server/app/controllers/oauth2.server.controller.js
+++ CS_496/assignment3/server/app/controllers/oauth2.server.controller.js
@@ -42,8 +42,13 @@ exports.oauth2 = function(req, res) {
                 var currentUserInfo = new userInfo();
                 currentUserInfo.Connect(tokenObject, function(userData) {
                     console.log ("Rendering signin...");
+
+                    //////////////////
+                    // Super test!
+                    //userData =JSON.parse('{"kind":"plus#person","etag":"_Ek1oAZn65JmcnNnmHdEAQCMkKA/VbM_lLCb2EYnVV2lCbFVq46IQKA","gender":"male","emails":[{"value":"silverevo03@gmail.com","type":"account"}],"urls":[{"value":"http://www.youtube.com/user/DirtMcGurtskie","type":"otherProfile","label":"Jeremy Prater"}],"objectType":"person","id":"116626949997452353203","displayName":"Jeremy Prater","name":{"familyName":"Prater","givenName":"Jeremy"},"tagline":"Aloha!","url":"https://plus.google.com/116626949997452353203","image":{"url":"https://lh6.googleusercontent.com/-GCdPoQeYXho/AAAAAAAAAAI/AAAAAAAAAiw/6fGb361gx9Q/photo.jpg?sz=50","isDefault":false},"organizations":[{"name":"Georgia State University","title":"Computer Science","type":"school","startDate":"2001","endDate":"2002","primary":false},{"name":"United States Navy","title":"Electrician","type":"work","startDate":"2004","primary":true}],"placesLived":[{"value":"96706","primary":true}],"isPlusUser":true,"circledByCount":35,"verified":false}');
                     console.log (`Token Object [${JSON.stringify(userData)}]`);
-                    var html = pug.renderFile('./app/views/oauth2.pug', {"userData": userData});
+
+                    var html = pug.renderFile('./app/views/oauth2.pug', userData);
                     res.send(html);
                 });
             });

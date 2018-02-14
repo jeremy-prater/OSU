0ed8b646cf585cf4bf20fd21d28790691057b7ee
Jeremy Prater <JPrate@polarisind.com>
2018-02-13 22:38:48 -0800

    - CS 496 - Assignment 4 - Android UI

---
+++ CS_496/assignment4/app/src/main/java/edu/osu/praterj/cs496androidui/MainActivity.java
@@ -0,0 +1,61 @@
+package edu.osu.praterj.cs496androidui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScreenActivity1.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScreenActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScreenActivity3.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScreenActivity4.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button5 = findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScreenActivity5.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ CS_496/assignment4/app/src/main/java/edu/osu/praterj/cs496androidui/ScreenActivity5.java
@@ -0,0 +1,12 @@
+package edu.osu.praterj.cs496androidui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ScreenActivity5 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity5);
+    }
+}
